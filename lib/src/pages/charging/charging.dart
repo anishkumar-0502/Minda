090c79a7a8258b8d79e8f1058e3521786e4f8dad
Anish <anishkumarak8686@gmail.com>
2024-07-17 11:06:39 +0530

    Revert "init"
    This reverts commit c6deaaba6f985a00b5e9653448666cff1c51170c.

--- lib/src/pages/charging/charging.dart
+++ lib/src/pages/charging/charging.dart
@@ -224,8 +224,6 @@ Future<void> updateSessionPriceToUser() async {
   try {
     handleAlertLoadingStart(context); // Show loading dialog
 
-    await Future.delayed(const Duration(seconds: 2));
-
     var url = Uri.parse('http://122.166.210.142:8052/getUpdatedCharingDetails');
     var body = {
       'chargerID': chargerID,
